3ea3fc0a336a4c28511462b766b27a6b0f6f2240
corey-marchand <corey.marchand@me.com>
2020-06-17 16:28:13 -0700

    crud complete

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView, CreateView, UpdateView, DeleteView, ListView
+from django.urls import reverse_lazy
+from .models import Blog
+
 # Create your views here.
 
-class HomeView(TemplateView):
-    template_name = 'home.html'
+class BlogListView(ListView):
+    template_name = 'base.html'
+    model = Blog
 
+class BlogDetailView(DetailView):
+    template_name = 'blog_details.html'
+    model = Blog
 
-#1 create the view
-#2 add a url
-#3 create a html page
+class BlogCreateView(CreateView):
+    template_name = 'blog_create.html'
+    model = Blog
+    fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+    template_name = 'blog_update.html'
+    model = Blog
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    template_name = 'blog_delete.html'
+    model = Blog
+    success_url = reverse_lazy('blog_list')
